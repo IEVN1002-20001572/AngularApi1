506cc3dd316f6e11ba28b7993b1b50dc7ab5f80c
Oscar Fernando Barajas Fuentes <20001572@alumnos.utleon.edu.mx>
2024-10-23 21:06:30 -0600

    primer commit api 1

---
+++ src/app/tem/temah/temah.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-temah',
+  standalone: true,
+  imports: [],
+  templateUrl: './temah.component.html',
+  styles: ``
+})
+export class TemahComponent {
+  @Input() mensaje!: string;
+  @Output() mensaje2 = new EventEmitter<string>();
+  enviarMensaje(){
+    this.mensaje2.emit('Hola mundo desde hijo');
+  }
+}
